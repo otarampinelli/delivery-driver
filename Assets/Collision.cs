0958cd184ad30ce8c839de43ab5a1df599977d20
otarampinelli <otaviorampinellielias@gmail.com>
2022-07-03 16:22:00 -0300

    first commit

---
+++ Assets/Collision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Collison here!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Trigger something");
+    }
+}
