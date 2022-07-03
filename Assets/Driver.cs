4184c26eed786468fcceb94ee8d5e28fa95e07f7
otarampinelli <otaviorampinellielias@gmail.com>
2022-07-03 19:37:35 -0300

    feat: creating a scenario

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -19,6 +19,6 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -steerAmount);
-        transform.Translate(0, -moveAmount, 0);
+        transform.Translate(0, moveAmount, 0);
     }
 }
